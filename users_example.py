aace75f6c4a10471847a16eaeb58bdbcd53e956f
InterSight <99773867+intersightlab@users.noreply.github.com>
2022-02-23 13:12:46 -0500

    for lab
    for lab

--- users_example.py
+++ users_example.py
@@ -1,40 +1,16 @@
-import logging
-from pprint import pformat
-import traceback
-
-import intersight.api.iam_api
+import intersight.api.iam_api as iam
 import credentials
 
-FORMAT = '%(asctime)-15s [%(levelname)s] [%(filename)s:%(lineno)s] %(message)s'
-logging.basicConfig(format=FORMAT, level=logging.DEBUG)
-logger = logging.getLogger('openapi')
-
-
-def get_users(apiClient):
-    ###################################################################################
-    api_instance = intersight.api.iam_api.IamApi(apiClient)
-    logger.info("Query Users")
-    results = api_instance.get_iam_user_list()
-    logger.info("User response: %s" % pformat(results))
-    logger.info("ANCESTORS: %s" % results.results[0].ancestors)
-
-    logger.info("Query Users with expand")
-    results = api_instance.get_iam_user_list(expand='Ancestors')
-    logger.info("Users: {}", pformat(results))
-
-
 def main():
-    # Configure API key settings for authentication
-    apiClient = credentials.config_credentials()
+    # Configure for authentication
+    cred = credentials.config_credentials()
+    iapi = iam.IamApi(cred)
 
-    try:
-        # Get list of users
-        get_users(apiClient)
+    #results = iapi.get_iam_user_list()
+    results = iapi.get_iam_user_list()
+    print(results)
 
-    except intersight.OpenApiException as e:
-        logger.error("Exception when calling API: %s\n" % e)
-        traceback.print_exc()
-
+   
 
 if __name__ == "__main__":
     main()
